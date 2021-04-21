ce2ff71bf39ef07a5e5de4f58a1ae97ea2dd2219
Jaya-272009 <66358077+Jaya-272009@users.noreply.github.com>
2021-04-21 21:32:01 +0530

    consultant form

--- src/Containers/ConsultantRegistration/ConsultantRegistration.js
+++ src/Containers/ConsultantRegistration/ConsultantRegistration.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import classes from "../../Containers/RegistrationForm.module.css"
+import { Col, Row,Input,Radio,Button,Select } from 'antd'
+
+const { Option } = Select;
 
 export default function ConsultantRegistration() {
     return (
+        <>
+        <div className={classes.body}>
+            <div className={classes.formTitle}>
+                <h1>Register As A Donor</h1>
+            </div>
+            <Row justify="center" >
+                <Col className={classes.formBox} lg={8} sm={16} xs={23}>
         <div>
-           ConsultantRegistration 
+            <div className={classes.formField}>
+                <p className={classes.title}>
+                    name
+                </p>
+                <Input type='text'   className={classes.inputField} placeholder="Enter your Name"  />
+            </div>
+            <div className={classes.formField}>
+                <p className={classes.title}>
+                    sex
+                </p>
+                <Radio.Group buttonStyle="solid" size='large' className={classes.radioGroup}>
+                    <Radio.Button className={classes.radioButton} value="male">Male</Radio.Button>
+                    <Radio.Button className={classes.radioButton} value="female">Female</Radio.Button>
+                    <Radio.Button className={classes.radioButton} value="others">Others</Radio.Button>
+                </Radio.Group>
+
+            </div>
+            <div className={classes.formField}>
+                <p className={classes.title}>
+                    mobile number
+                </p>
+                <Input type='number'   className={classes.inputField} placeholder="Enter your Mobile Number"  />
+            </div>
+            <div className={classes.formField}>
+                <p className={classes.title}>
+                    select your state
+                </p>
+                
+                
+
+            </div>
         </div>
+        <Button block className={classes.Button}>
+                        Register Now
+                    </Button>
+        </Col>
+        </Row>
+        </div>
+
+        
+    </>
     )
 }
