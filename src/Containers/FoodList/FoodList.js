167bab5462e69497585fae565f941f33f49a455a
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-04-22 19:17:54 +0530

    List pages updated

--- src/Containers/FoodList/FoodList.js
+++ src/Containers/FoodList/FoodList.js
@@ -1,5 +1,89 @@
 import React from "react";
+import InfoCard from "../../Components/InfoCard/InfoCard";
+import classes from "../RegistrationForm.module.css";
+import { Col, Row } from "antd";
 
+const foodList = [
+  {
+    name: "Ritik Gupta",
+    phone: "+91-80107080479",
+    location: "Delhi",
+  },
+  {
+    name: "Sourodeep ghosh roy",
+    phone: "+941233588",
+    location: "Delhi",
+  },
+  {
+    name: "Jaya gaur",
+    phone: "+9564665161",
+    location: "Delhi",
+  },
+  {
+    name: "Manas Srivastava",
+    phone: "+9578626485",
+    location: "Delhi",
+  },
+  {
+    name: "Ritik Gupta",
+    phone: "+91-80107080479",
+    location: "Delhi",
+  },
+  {
+    name: "Sourodeep ghosh roy",
+    phone: "+941233588",
+    location: "Delhi",
+  },
+  {
+    name: "Jaya gaur",
+    phone: "+9564665161",
+    location: "Delhi",
+  },
+  {
+    name: "Manas Srivastava",
+    phone: "+9578626485",
+    location: "Delhi",
+  },
+  {
+    name: "Dr. XYZ",
+    phone: "2646161646",
+    location: "Delhi",
+  },
+
+  {
+    name: "Jaya gaur",
+    phone: "+9564665161",
+    location: "Delhi",
+  },
+  {
+    name: "Manas Srivastava",
+    phone: "+9578626485",
+    location: "Delhi",
+  },
+  {
+    name: "Dr. XYZ",
+    phone: "2646161646",
+    location: "Delhi",
+  },
+];
 export default function FoodList() {
-  return <div>Food List comes here</div>;
+  return (
+    <div className={classes.body}>
+      <div className={classes.formTitle}>
+        <h1>Food Delivery</h1>
+      </div>
+      <Row justify='center' gutter={[8, 8]}>
+        {foodList.map((food, index) => (
+          <Col lg={7} md={8} sm={15} xs={24}>
+            <InfoCard
+              name={food.name}
+              phone={food.phone}
+              type='food'
+              location={food.location}
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
