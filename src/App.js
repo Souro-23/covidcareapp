c1a7d1c30acf2369eeb43a1bc36cba7b30ad41af
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-04-30 10:46:11 +0530

    Hostng Essentials Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {useEffect} from 'react'
 import Root from './Containers/Root';
 import "antd/dist/antd.css"; 
 import { BrowserRouter } from "react-router-dom";
+import firebase from './Firebase/FirebaseConfig'
+import 'firebase/analytics';
 
 function App() {
+  useEffect(() => {
+    firebase.analytics()
+  }, [])
   return ( 
     <BrowserRouter>
       <Root />
