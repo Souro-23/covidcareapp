10bf1245ff18d33c6ec05c343989d9a113d4432f
Ritik Gupta <ritikgupta1982@gmail.com>
2021-05-03 02:07:43 +0530

    Section 3 Started

--- src/Containers/Home/Section3.js
+++ src/Containers/Home/Section3.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button } from "antd";
+import React from "react";
+import classes from "./Section3.module.css";
 
 export default function Section3() {
-    return (
-        <div>
-            Fight anxiety & STRESS
-        </div>
-    )
+  return (
+    <div className={classes.section3Card}>
+      <p>FIGHT ANXIETY</p>
+      <p>& STRESS</p>
+      <p>FREE SESSIONS</p>
+      <p>ON BREATHING TECHNIQUES</p>
+      <Button className={classes.section3Button}>I'm Intrested</Button>
+    </div>
+  );
 }
